955ab36dd2cd49ed1e137eb51e2555a46ece43bf
helkbore <helkbore@163.com>
2018-05-03 17:09:08 +0800

    添加了一些js库

---
+++ js/testjs/UItest/js/hplus.js
@@ -0,0 +1,18 @@
+// 自定义 js
+
+// 公共配置
+
+$(document).ready(function(){
+    // MetsiMenu
+    $('#side-menu').metisMenu();
+});
+
+$(window).bind();
+
+function NavToggle() {}
+
+function SmoothlyMenu() {}
+
+$(function(){});
+
+function localStorageSupport() {}
